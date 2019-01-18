4185e5870cb0754dc7eb83ab3b71ed389e29f12a
Cliff Duffey <29979827+cduffey@users.noreply.github.com>
2019-01-18 11:13:48 -0500

    finished ninja 1

---
+++ 01-javascript/03-js-oop/01-ninja-class-i.js
@@ -0,0 +1,25 @@
+function Ninja(name) {
+    var speed = 3;
+    var strength = 3;
+
+    this.health = 100;
+    this.name = name;
+
+    this.sayName = function() {
+        console.log("My ninja name is " + this.name);
+    }
+
+    this.showStats = function() {
+       console.log("Name: " + this.name + " Health: " + this.health + " Speed: " + speed + " Strength: " + strength); 
+    } 
+
+    this.drinkSake = function() {
+        this.health += 10;
+        return this;
+    }
+}
+
+var ninja1 = new Ninja("Hyabusa");
+ninja1.sayName();
+// -> "My ninja name is Hyabusa!"
+ninja1.showStats();
